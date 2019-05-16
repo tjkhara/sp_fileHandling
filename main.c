de0c1771b406ae412f67bc63874eb62f96870287
Tajeshwar Khara <tkhara@gmail.com>
2019-05-16 10:28:55 +0530

    Writing characters using fputc

--- main.c
+++ main.c
@@ -2,6 +2,29 @@
 
 int main()
 {
+    // File pointer
+    FILE *fp;
+
+    fp = fopen("a.txt", "a");
+
+    if(fp == NULL)
+    {
+        printf("Unable to open file\n");
+        return 1;
+    }
+    // file opened successfully
+
+    char ch;
+
+    for(ch = 'a'; ch <= 'z'; ch++)
+    {
+        fputc(ch, fp);
+    }
+
+    // Close the file and flush the content into the file
+    fclose(fp);
+
+    printf("Success\n");
 
     return 0;
 }
