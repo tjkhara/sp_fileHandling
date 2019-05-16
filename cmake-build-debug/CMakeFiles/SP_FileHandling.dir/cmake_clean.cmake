88a72de24d94762e711de3319a3605a76646b3fd
Tajeshwar Khara <tkhara@gmail.com>
2019-05-16 10:11:39 +0530

    Initial commit file handling

---
+++ cmake-build-debug/CMakeFiles/SP_FileHandling.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SP_FileHandling.dir/main.c.o"
+  "SP_FileHandling.pdb"
+  "SP_FileHandling"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SP_FileHandling.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
